d38528cd62a0425f8cffc8e812f8df4d403fe747
Flavio Lima <ctrlserr@gmail.com>
2025-11-19 21:25:56 -0300

    fix: remove redirecionamento para site.prontupsi.com da rota raiz
    - Rota raiz (/) agora vai para LandingPage
    - Remove ExternalRedirect para site externo
    - UsuÃ¡rios ficam no app em vez de serem redirecionados

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import ExternalRedirect from "./components/ExternalRedirect";
+// import ExternalRedirect from "./components/ExternalRedirect";
 import { AuthProvider } from "./components/auth/AuthProvider";
 import { ProtectedRoute } from "./components/auth/ProtectedRoute";
 import { PublicRoute } from "./components/auth/PublicRoute";
@@ -51,7 +51,7 @@ function App() {
                 <AuthProvider>
                 <Routes>
                   {/* Páginas públicas */}
-                  <Route path="/" element={<ExternalRedirect to="https://site.prontupsi.com" />} />
+                  <Route path="/" element={<LandingPage />} />
                   <Route 
                     path="/login" 
                     element={
